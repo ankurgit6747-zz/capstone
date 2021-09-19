0350adcadf3ec0843b7ae540e2509348e0a8c20d
honeybharatandani <andanihoney106@gmail.com>
2021-09-19 21:52:43 +0530

    updates

--- frontend/src/components/Working.js
+++ frontend/src/components/Working.js
@@ -1,9 +1,9 @@
 import "antd/dist/antd.css";
 import { Modal, Button } from "antd";
 import React, { useState, useEffect } from "react";
-import work from "../svg/work.svg";
-import { Image } from "react-bootstrap";
 import process from "../svg/process.svg";
+import { Image } from "react-bootstrap";
+import thoughts from "../svg/thoughts.svg";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
@@ -26,17 +26,28 @@ const Working = () => {
   };
 
   const contentStyle2 = {
-    marginLeft: "100px",
-    marginTop: "80px",
+    display: "flex",
+  };
+  const contentStyle4 = {
+    marginLeft: "280px",
+    marginTop: "300px",
   };
 
   const contentStyle3 = {
-    marginLeft: "500px",
-    marginTop: "200px",
+    marginLeft: "300px",
+    marginTop: "300px",
   };
   return (
     <div style={contentStyle2}>
+      <Image
+        data-aos="fade-down"
+        data-aos-duration="1500"
+        width={300}
+        src={process}
+      />
+
       <Button
+        style={contentStyle4}
         data-aos="fade-down"
         data-aos-easing="linear"
         data-aos-duration="1500"
@@ -51,7 +62,6 @@ const Working = () => {
         onOk={handleOk}
         onCancel={handleCancel}
       >
-        <Image width={200} src={process} />
         <hr />
         <p>User has to first Login to the website</p>
         <hr />
@@ -60,16 +70,13 @@ const Working = () => {
         <p>............</p> <hr />
         <p>............</p> <hr />
         <p>............</p> <hr />
-        {/* <p>............</p> <hr />
         <p>............</p> <hr />
-        <p>............</p> <hr />
-        <p>............</p> <hr /> */}
       </Modal>
       <Image
         data-aos="fade-left"
         style={contentStyle3}
         width={400}
-        src={work}
+        src={thoughts}
       />
     </div>
   );
